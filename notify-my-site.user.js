3aa22aef56db5240f8c835b55a8039380cee8f29
Matheus Henrique <matheushenriquedev@hotmail.com>
2024-04-08 17:37:41 -0300

    new version

--- notify-my-site.user.js
+++ notify-my-site.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Notification My Site
 // @namespace    http://matheushenrique.dev/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Notification in Site
 // @author       Matheus
 // @updateURL    https://raw.githubusercontent.com/maatheeus/notification-my-site/main/notify-my-site.user.js
